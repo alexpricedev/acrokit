58e0e7e2575eb23307da59439badc2f5dcc9076d
Alex Price (pi-500) <github@alexprice.dev>
2025-07-07 21:01:54 +0100

    Add debug component to inspect pose data

--- src/components/FlowBuilder.tsx
+++ src/components/FlowBuilder.tsx
@@ -7,6 +7,7 @@ import { PoseDetailModal } from './PoseDetailModal';
 import { useAuth } from './AuthProvider';
 import { useToast } from './ToastProvider';
 import { useFlowData, useFavorites } from '../hooks';
+import { DebugPoses } from './DebugPoses';
 
 interface FlowBuilderProps {
   initialFlow?: FlowStep[];
@@ -235,6 +236,7 @@ export function FlowBuilder({ initialFlow, editingFlowId }: FlowBuilderProps) {
 
   return (
     <div className="max-w-7xl mx-auto p-4 sm:p-6">
+      <DebugPoses />
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 lg:gap-8">
         {/* Left Column - Your Flow */}
         <div className="lg:col-span-1">
